a71bd675dea82606054e77029e04cd2a9d1c4a3f
Kathy <kath.scholle@gmail.com>
2023-01-18 16:16:20 -0500

    wip: basic function done

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,1 @@
 //alert("working");
-
-//making the img clickable
-$(document).ready(function() {
-    $("#img").on("click", handleGetInfo);
-    function handleGetInfo(evt){ 
-    alert("You clicked the img"); 
-    };
-
-});
--- server.js
+++ server.js
@@ -2,11 +2,21 @@
 const express = require('express');
 const pokemonList = require('./models/pokemon.js');
 const port = 3000;
+const methodOverride = require('method-override'); //origin line 5
 //initialize the express application
 const app = express();
 
 //configure database
 const pokemons = require('./models/pokemon.js');
+
+//gives us access to a special object called req.body
+//req.body is used to gather form input, anytime an input is submitted, it becomes req.body
+//take info from form and update the form
+app.use(express.urlencoded({extended: false}))
+
+app.use(methodOverride('_method')); //invoking the methodOverride method, need line 5 to work
+//this takes a special query parameter
+//this way it knows which request needs to be overridden(in this case, any method that includes'_method')
 
 //static middleware takes the line 9 in index.ejs go into "public" folder
 app.use(express.static('public')); 
@@ -28,12 +38,33 @@ app.get('/pokemons/new', function(req, res){
 })
 
 //delete - DELETE - /pokemons/:index
+app.delete('/pokemons/:index', function(req, res){
+    pokemonList.splice(req.params.index, 1);
+    res.redirect('/pokemons');
+})
 
 //update - PUT /pokemons/:index
+app.put('/pokemons/:index', function(req, res){
+    pokemonList[req.params.index] = req.body;
+    console.log(req.body);
+    res.redirect('/pokemons');
+})
 
 //create - POST /pokemons - take form data and create a new pokemon with it
+app.post('/pokemons', function(req, res){
+    pokemonList.push(req.body);
+    console.log(req.body);
+    res.redirect('/pokemons');
+});
 
 //edit - GET /pokemons/:index/edit - sending a page that allows a user to edit a pokemon
+app.get('/pokemons/:index/edit', function(req, res){
+    res.render('edit.ejs', {
+       pokemonE: pokemonList[req.params.index],
+       index: req.params.index, //to use in edit.ejs line 11 //original line 57
+       title: 'Edit ' + pokemonList.name 
+    });
+});
 
 //show - GET /pokemons/:someUniqueIndentifier
 app.get('/pokemons/:index', function(req, res){
